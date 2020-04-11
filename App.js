49d5825230aa1da2fe0df252de03e5dc9207e835
Andrew Mayer <andrewmayer515@gmail.com>
2020-04-11 11:00:50 -0500

    project structure

--- App.js
+++ App.js
@@ -6,8 +6,8 @@ import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import useLinking from './navigation/useLinking';
+import BottomTabNavigator from './src/navigation/BottomTabNavigator';
+import useLinking from './src/navigation/useLinking';
 
 const Stack = createStackNavigator();
 
